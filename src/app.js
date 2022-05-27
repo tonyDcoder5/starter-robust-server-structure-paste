6f8c25786c6bed59e19f7c1580a90ff0fa08c05e
TonyDCoder5 <tonyaflex5@gmail.com>
2022-05-27 14:06:52 -0600

    module 31.6 organizing react code using Router

--- src/app.js
+++ src/app.js
@@ -1,59 +1,10 @@
 const express = require("express");
 const app = express();
-
+const pastesRouter = require("./pastes/pastes.router");
 app.use(express.json())
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
-const pastes = require("./data/pastes-data");
-
-app.use("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
-
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next({ status: 404, message: `Paste id not found: ${pasteId}` });
-  }
-});
-
-app.get("/pastes", (req, res) => {
-  res.json({ data: pastes });
-});
-
-// New middleware function to validate the request body
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next();
-  }
-  next({
-    status: 400,
-    message: "A 'text' property is required.",
-  });
-}
-
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-
-app.post(
-  "/pastes",
-  bodyHasTextProperty, // Add validation middleware function
-  (req, res) => {
-    // Route handler no longer has validation code.
-    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-    const newPaste = {
-      id: ++lastPasteId, // Increment last id then assign as the current ID
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    };
-    pastes.push(newPaste);
-    res.status(201).json({ data: newPaste });
-  }
-);
+// API Endpoints
+app.use("/pastes", pastesRouter); // Note: app.use
 
 // Not found handler
 app.use((request, response, next) => {
