6445e03dc4aed693f624ccc22789f01f2c90e42a
TonyDCoder5 <tonyaflex5@gmail.com>
2022-05-27 12:04:00 -0600

    module 31.2 lesson changes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+const pastes = require("./data/pastes-data");
+
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
 
 // Not found handler
 app.use((request, response, next) => {
