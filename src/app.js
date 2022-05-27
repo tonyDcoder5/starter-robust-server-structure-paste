ec666ecf6dd68f84c066b72b90592ed6d724e6a0
TonyDCoder5 <tonyaflex5@gmail.com>
2022-05-27 12:39:48 -0600

    module 31.5 error types and handling

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${pasteId}`);
+    next({ status: 404, message: `Paste id not found: ${pasteId}` });
   }
 });
 
@@ -21,27 +21,39 @@ app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
 
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
+// New middleware function to validate the request body
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "A 'text' property is required.",
+  });
+}
+
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.post("/pastes", (req, res, next) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-  if (text) {
+app.post(
+  "/pastes",
+  bodyHasTextProperty, // Add validation middleware function
+  (req, res) => {
+    // Route handler no longer has validation code.
+    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
     const newPaste = {
-      id: ++lastPasteId, // Increment last ID, then assign as the current ID
+      id: ++lastPasteId, // Increment last id then assign as the current ID
       name,
       syntax,
       exposure,
       expiration,
       text,
+      user_id,
     };
     pastes.push(newPaste);
     res.status(201).json({ data: newPaste });
-  } else {
-    res.sendStatus(400);
   }
-});
+);
 
 // Not found handler
 app.use((request, response, next) => {
@@ -49,9 +61,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
